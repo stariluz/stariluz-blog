c1aefc60ba7dd688ea8070a42dec0c8064ce72e8
Adora <im.stariluz@gmail.com>
2025-01-26 20:56:30 -0600

    Add reponame to fetch src

--- src/components/PostView/PostView.tsx
+++ src/components/PostView/PostView.tsx
@@ -27,7 +27,7 @@ const PostView = () => {
         const fetchPost = async () => {
             try {
                 // Carga el contenido del archivo Markdown
-                const response = await fetch(`/posts/${date}.md`);
+                const response = await fetch(`${import.meta.env.VITE_REPO_NAME ? '/' + import.meta.env.VITE_REPO_NAME : ''}/posts/${date}.md`);
                 // Si no se encuentra en el JSON, intenta cargarlo desde el archivo Markdown
                 const markdown = await response.text();
                 setFileContent(markdown);
@@ -53,7 +53,7 @@ const PostView = () => {
     return (
         <div className="post-view">
             <MarkdownParser markdown={fileContent} classnames={{ h1: "title", blockquote: 'quote', code: 'code' }} />
-            
+
             <Link to={`..`} className="back-btn svg-baseline">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-circle-arrow-left"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 21a9 9 0 1 0 0 -18a9 9 0 0 0 0 18" /><path d="M8 12l4 4" /><path d="M8 12h8" /><path d="M12 8l-4 4" /></svg>
             </Link>

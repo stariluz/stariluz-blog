f9b2dfbc1ec234ba5f2af4d3dd5b1153f793106c
Adora <im.stariluz@gmail.com>
2025-01-26 20:49:51 -0600

    Debug reponame

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ }) => {
+  console.log("VITE_REPO_NAME:", process.env.VITE_REPO_NAME);
+  return {
+    plugins: [react()],
 
-  resolve: {
-    alias: {
-      'src': fileURLToPath(new URL('./src', import.meta.url))
+    resolve: {
+      alias: {
+        'src': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
 
-  base: process.env.NODE_ENV === 'production'
-    ? process.env.VITE_REPO_NAME || '/'
-    : '/',
+    base: process.env.NODE_ENV === 'production'
+      ? `${process.env.VITE_REPO_NAME}` || '/'
+      : '/',
+  }
 })

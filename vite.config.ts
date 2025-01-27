dfdaba618329e2c85dc9be2f6f2a04ebd7548412
Adora <im.stariluz@gmail.com>
2025-01-26 20:08:33 -0600

    Try to deploy 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   watch: {
-  //     usePolling: true
-  //   }
-  // }
+
+  resolve: {
+    alias: {
+      'src': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+
+  base: process.env.NODE_ENV === 'production'
+    ? process.env.VITE_REPO_NAME || '/'
+    : '/',
 })
